70734e2ae7c0f33bfb01517e71d44be501574de1
MAL5002 <93272740+MAL5002@users.noreply.github.com>
2023-10-13 17:31:28 +0100

    Update keymap.c

--- keyboards/keychron/q1_pro/iso_knob/keymaps/mine/keymap.c
+++ keyboards/keychron/q1_pro/iso_knob/keymaps/mine/keymap.c
@@ -88,11 +88,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
-        for (uint8_t i = led_min; i < led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, 193, 225, 193);
-            }
-        }
+        rgb_matrix_set_color(45, 193, 255, 193);
     }
     return false;
 }
