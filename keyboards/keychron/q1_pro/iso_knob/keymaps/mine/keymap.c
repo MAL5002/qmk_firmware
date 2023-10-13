546afbb106f31a425909549316314a0de72392f5
MAL5002 <93272740+MAL5002@users.noreply.github.com>
2023-10-13 16:29:59 +0100

    Update keymap.c

--- keyboards/keychron/q1_pro/iso_knob/keymaps/mine/keymap.c
+++ keyboards/keychron/q1_pro/iso_knob/keymaps/mine/keymap.c
@@ -85,3 +85,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
     return true;
 }
+
+void rgb_matrix_indicators_user(void) {
+    if (IS_LED_HOST_ON(USB_LED_CAPS_LOCK)) {
+        rgb_matrix_set_color( 45, r, g, b);
+    }
+}
